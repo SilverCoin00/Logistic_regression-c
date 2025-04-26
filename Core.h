e9f613e269291ce9118f85aa81d6f88edd7a9f8f
SilverCoin00 <dang.nguyen.sp@gmail.com>
2025-04-26 15:39:39 +0700

    Update Core.h

--- Core.h
+++ Core.h
@@ -6,10 +6,10 @@
 #include <string.h>
 #include <math.h>
 
-#include "D:\Data\code_doc\AI_model_building\Pandas_&_Numpy.c"
-#include "D:\Data\code_doc\AI_model_building\Sklearn.c"
-#include "D:\Data\code_doc\AI_model_building\Dataset.c"
-#include "D:\Data\code_doc\AI_model_building\Logistic_regression\Weights.c"
-#include "D:\Data\code_doc\AI_model_building\Logistic_regression\Logistic_regression.c"
+#include "Pandas_&_Numpy.c"
+#include "Sklearn.c"
+#include "Dataset.c"
+#include "Weights.c"
+#include "Logistic_regression.c"
 
 #endif
